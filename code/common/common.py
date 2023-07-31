eab3c0da865f2ecb1dc01d0da01e452d30ca89f2
fsternini <f.sternini@gmail.com>
2023-07-31 16:04:33 +0200

    prova environment

--- code/common/common.py
+++ code/common/common.py
@@ -1,15 +1,19 @@
-import json
-import dicom
 import os
+import json
 
 class DatasetCreator:
     """Returns a dictionary with all dataset resources and respective paths.
     """
-    
-    def __init__(self, path):
+
+    def __init__(self, path, archivepath = None):
         self.path = path
+        if archivepath is None:
+            self.archive_path = self.path
+        else:
+            self.archive_path = archivepath
         self.dataset = {}
         self.generate_dataset()
+        self.archive()
 
     def generate_dataset(self):
         for root, directories, files in os.walk(self.path):
@@ -19,3 +23,10 @@ class DatasetCreator:
                 current_node = current_node.setdefault(folder, {})
             for file in files:
                 current_node[file] = os.path.join(root,file)
+
+    def archive(self):
+        with open(self.archive_path+'dataset_structure.json','x') as fp:
+            json.dump(self.dataset,fp)
+
+if __name__ == '__main__':
+    pass
